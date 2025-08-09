3cc4e482f926e6ab08b1e556e0b8152babadbf55
nskilleer <122985929+nskilleer@users.noreply.github.com>
2025-08-09 20:16:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
 
+
 // =====================================================
 // Importa middlewares customizados
 // =====================================================
