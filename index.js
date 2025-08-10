ab9e4af33b6887eaff5d8a88983fa9708933056f
nskilleer <122985929+nskilleer@users.noreply.github.com>
2025-08-09 21:43:02 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Arquivo: index.js
 
-
 require('dotenv').config({ path: './variaveisambiente.env' });
 const app = require('./app');
 const logger = require('./configuracoes/logger');
@@ -8,21 +7,23 @@ const { connectDB } = require('./middlewares/dbMiddleware');
 
 const port = process.env.PORT || 3333;
 
-// Exporta o handler para Vercel
-module.exports = app;
+// Função de inicialização (roda tanto no local quanto na Vercel)
+(async () => {
+    try {
+        await connectDB();
 
-// Inicialização local (apenas se não estiver no ambiente Vercel)
-if (process.env.VERCEL !== '1') {
-    (async () => {
-        try {
-            await connectDB();
+        // Se for ambiente local, inicia o servidor
+        if (process.env.VERCEL !== '1') {
             app.listen(port, () => {
                 logger.info(`Servidor rodando na porta ${port}`);
                 logger.info(`Servidor rodando: http://localhost:${port}`);
             });
-        } catch (error) {
-            logger.error('Falha ao iniciar o servidor:', error);
-            process.exit(1);
         }
-    })();
-}
+    } catch (error) {
+        logger.error('Falha ao iniciar o servidor:', error);
+        process.exit(1);
+    }
+})();
+
+// Exporta o app para que a Vercel possa gerenciar as requisições
+module.exports = app;
